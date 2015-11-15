9c47ef86a2cb55ed4f39dac2aa03445e59a83876
nemu0222 <nori02221994@gmail.com>
2015-11-15 12:04:02 +0900

    敵をクリックすると敵が消えて、パーティクル生成

---
+++ 2015-11-14-unityproj/Assets/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// enemy関数
+public class Enemy : Token
+{
+	// 初期化
+	void Start()
+	{
+		// enemyのサイズ設定
+		SetSize(SpriteWidth / 2, SpriteHeight / 2);
+		// ランダムな方向に移動
+		// 方向をランダムに決める
+		float dir = Random.Range(0,359);
+		
+		// 速さ
+		float speed = 2;
+		SetVelocity(dir,speed);
+	}
+
+	// 更新
+	void Update()
+	{
+		// カメラの左下座標取得
+		Vector2 min = GetWorldMin();
+		// カメラの右上座標取得
+		Vector2 max = GetWorldMax();
+
+		if (X < min.x || max.x < X)
+		{
+			// x座標が画面外
+			VX *= -1;
+			// 画面内に移動
+			ClampScreen();
+		}
+
+		if (Y < min.y || max.y < Y)
+		{
+			// y座標が画面外
+			VY *= -1;
+			// 画面内に移動
+			ClampScreen();
+		}
+	}
+
+	// クリック処理
+	public void OnMouseDown()
+	{
+		for (int i = 0; i < 32; i++)
+		{
+			Particle.Add(X,Y);
+		}
+
+		// オブジェクト破棄
+		DestroyObj();
+	}
+}
+
+// EOF
---
+++ 2015-11-14-unityproj/Assets/Scripts/Particle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : Token
+{
+	// プレハブ
+	static GameObject _prefab = null;
+	// パーティクル生成
+	public static Particle Add(float x,float y)
+	{
+		// プレハブ取得
+		_prefab = GetPrefab(_prefab,"Particle");
+		// プレハブからインスタンス生成
+		return CreateInstance2<Particle>(_prefab,x,y);
+	}
+
+	// 初期化
+	IEnumerator Start()
+	{
+		// 移動方向と速さをランダムに決める
+		float dir = Random.Range(0, 359);
+		float spd = Random.Range(10.0f, 20.0f);
+		SetVelocity(dir, spd);
+		
+		// 見えなくなるまで小さく
+		while (ScaleX > 0.01f)
+		{
+			// 0.01秒ゲームループに制御返す
+			yield return new WaitForSeconds(0.01f);
+			// だんだん小さく
+			MulScale(0.9f);
+			// だんだん減速
+			MulVelocity(0.9f);
+		}
+		
+		// オブジェクト破棄
+		DestroyObj();		
+	}
+	
+	// 更新
+	void Update()
+	{
+	
+	}
+}
+
+// EOF
